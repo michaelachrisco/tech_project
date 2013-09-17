f958491e0ad156da668780e1855b5c2fa2076c35
mesa <mesa@LapLap.(none)>
2013-09-17 11:48:16 -0700

    Initial project. Alternate library and initial work done.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TechProject::Application.config.secret_key_base = '4eaf27557afc6d751993f2ab5a275fdbf383f7cf54b9a4001fa750af56394034af911aa5d111825b315785e5e5bbab98aa4d38a3cfa7853389f661a96de31faa'
